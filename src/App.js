983784cc7113e9a853cf02337dc5f80505441f73
Duncan Moyo <duncanfmoyo@gmail.com>
2020-04-22 21:48:44 +0200

    understanding jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-     <h1>Hi i am a react App</h1>
-    </div>
+  // return (
+  //   <div className="App">
+  //    <h1>Hi i am a react App</h1>
+  //   </div>
+  // );
+
+  return React.createElement(
+    "div",
+    null,
+    React.createElement("h1", { className: "App" }, "I'm a React Thingy")
   );
 }
 
