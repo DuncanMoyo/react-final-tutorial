0e1da6f2bbbbe8f5257d6e18b64022fed1f12093
Duncan Moyo <duncanfmoyo@gmail.com>
2020-04-23 00:38:16 +0200

    creating a functional component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
+import Person from './Person/Person';
 
 function App() {
-  // return (
-  //   <div className="App">
-  //    <h1>Hi i am a react App</h1>
-  //   </div>
-  // );
-
-  return React.createElement(
-    "div",
-    null,
-    React.createElement("h1", { className: "App" }, "I'm a React Thingy")
+  return (
+    <div className="App">
+     <h1>Hi i am a react App</h1>
+     <Person />
+    </div>
   );
+
+  // return React.createElement('div', null, React.createElement('h1', {className:}, 'I\'m a React Thingy'));
 }
 
 export default App;
