b690a904aeda73c00d668628e4a6d6784dccc023
Duncan Moyo <duncanfmoyo@gmail.com>
2020-04-23 12:35:31 +0200

    handling events with methods

--- src/App.js
+++ src/App.js
@@ -1,31 +1,43 @@
-import React, {Component} from 'react';
-import './App.css';
-import Person from './Person/Person';
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
-
   state = {
     persons: [
-      {name: 'Duncan', age: 28},
-      {name: 'Naomi', age: 22},
-      {name: 'Moyo', age: 40},
-    ]
-  }
+      { name: "Duncan", age: 28 },
+      { name: "Naomi", age: 22 },
+      { name: "Moyo", age: 40 },
+    ],
+  };
+
+  switchNameHandler = () => {
+    console.log("was clicked");
+  };
 
   render() {
     return (
       <div className="App">
+        <h1>Hi i am a react App</h1>
 
-       <h1>Hi i am a react App</h1>
-
-       <button>Switch Name</button>
-       <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My hobbies: Cricket</Person>
-       <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-       <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        >
+          My hobbies: Cricket
+        </Person>
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
       </div>
     );
   }
- 
 
   // return React.createElement('div', null, React.createElement('h1', {className:}, 'I\'m a React Thingy'));
 }
