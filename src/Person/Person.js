ba2063a4dd88c87c630781f24a1422a561e8e2c0
Duncan Moyo <duncanfmoyo@gmail.com>
2020-04-23 10:50:57 +0200

    outputting dynamic content

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Person = () => {
-  return <p>I'm a person!</p>;
+  return <p>I'm a person and i am {Math.floor(Math.random() * 28)}  years old!</p>;
 };
 
 export default Person;
