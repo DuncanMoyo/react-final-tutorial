67f8bfd98df9091fde76cc9e99dac78236c1cbba
Duncan Moyo <duncanfmoyo@gmail.com>
2020-04-28 12:16:09 +0200

    using useEffect in Functional components

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, {useEffect} from "react";
 import classes from './Cockpit.css'
 
 const Cockpit = (props) => {
+
+  useEffect(() => {
+    console.log('[Cockpit.js] useEffect');
+    // Http request would be ok here
+  });
+
   const assignedClasses = [];
 
   let btnClass = "";
